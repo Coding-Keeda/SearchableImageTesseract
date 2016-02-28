5a6c35a31acae7834ac39e94b143d8704b0982e3
Coding Keeda <amitsb3747@gmail.com>
2016-02-29 01:53:29 +0530

    Update HomeController.cs

--- TesseractImageSearch/Controllers/HomeController.cs
+++ TesseractImageSearch/Controllers/HomeController.cs
@@ -6,6 +6,19 @@ using System.Web.Hosting;
 using System.Web.Mvc;
 using Tesseract;
 
+//# Intruction to install Tesseract wrapper
+
+//1. In your VS .Go to TOOLS --> NuGet Package Manager --> Package Manager Console
+//2. Write below line first in the console
+//3. < UnInstall-Package Tesseract >  and then
+//4. < Install-Package Tesseract > 
+
+//# Following tesseract C# wrapper has been used for this project 
+//https://www.nuget.org/packages/Tesseract/
+
+
+//For any query ,feel free to contact me at amitsb3747@gmail.com
+
 namespace TesseractImageSearch.Controllers
 {
     public class HomeController : Controller
